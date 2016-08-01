5736bbe550248cb9d396d503367305af7010f449
dave_elmo <dave.newson@elmolearning.com.au>
2016-08-01 16:05:53 +1000

    Added: featherlight image lightbox.

--- src/js/script.js
+++ src/js/script.js
@@ -4,18 +4,50 @@
  * Licensed under Apache 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
  */
 
-// Tooltip Init
-$(function() {
+jQuery(function() {
+
+    // Tooltip init
     $("[data-toggle='tooltip']").tooltip();
-});
 
-//make all images responsive
-$(function() {
-    $("img").addClass("img-responsive")
-});
+    // Make all images responsive
+    $("img").addClass("img-responsive");
 
-// Floating label headings for the contact form
-$(function() {
+    // Gallery lightbox for any image larger than spec
+    // Anything in an article
+    $('article img').each(function() {
+        var img = this;
+
+        // If we can determine naturalHeight (HTML5)
+        // Then don't use lightbox where the image is no bigger at native.
+        if (img.naturalHeight != undefined) {
+            if (img.naturalHeight <= img.height) {
+                return;
+            }
+        }
+
+        var $img = $(img);
+
+        // Get the link
+        var $link = $img.parent('a');
+        if ($link.length == 0) {
+
+            // Wrap with link if not set
+            var $linkSrc = $('<a />');
+            $linkSrc.attr('href', $img.attr('src'));
+            $img.wrap($linkSrc);
+
+            // Re-grab the link DOM element.
+            // Using Wrap clones the object, so $link is no longer the actual.
+            $link = $img.parent('a');
+        }
+
+        // Apply featherlight
+        $link.featherlight({
+            type: 'image'
+        });
+    });
+
+    // Floating label headings for the contact form
     $("body").on("input propertychange", ".floating-label-form-group", function(e) {
         $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
     }).on("focus", ".floating-label-form-group", function() {
@@ -23,10 +55,8 @@ $(function() {
     }).on("blur", ".floating-label-form-group", function() {
         $(this).removeClass("floating-label-form-group-with-focus");
     });
-});
 
-// Navigation Scripts to Show Header on Scroll-Up
-jQuery(document).ready(function($) {
+    // Navigation Scripts to Show Header on Scroll-Up
     var MQL = 1170;
 
     //primary navigation slide-in effect
